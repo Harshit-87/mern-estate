2333d1e2bdd9c48df9b21c1c46e7d47f97f8e4df
Harshit-87 <harshitsrivastava25012002@gmail.com>
2024-01-20 22:43:59 +0530

    add contact landlord functionality to listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -23,9 +24,11 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
-
+  const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
+
+  console.log(currentUser)
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -143,7 +146,7 @@ export default function Listing() {
                 Contact landlord
               </button>
             )}
-            
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
